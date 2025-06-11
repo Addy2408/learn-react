83ccbe93b8d5d3057b80925403b83c8d308cb028
Addy2408 <adityaverma2408@gmail.com>
2025-06-11 09:30:45 +0530

    learn tailwind and props

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,24 +2,34 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-
   let [counter, setCounter] = useState(0);
 
   const increase = () => {
-    if(counter >= 20) {
+    if (counter >= 20) {
       alert("Counter value cannot be greater than 20");
       return;
     }
+    // Directly updating the state variable
+    // This is not recommended as it does not guarantee the latest state
+    // counter = counter + 1; // This will not trigger a re-render
+    // Instead, we should use the setCounter function provided by useState
     setCounter(counter + 1);
-  }
+
+    // Using functional updates to ensure the latest state is used
+    // This is important when the state update depends on the previous state
+    // This is especially useful in cases where multiple updates are made in quick succession
+    // For example, if you want to increase the counter by 2
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+  };
 
   const decrease = () => {
-    if(counter <= 0) {
+    if (counter <= 0) {
       alert("Counter value cannot be less than 0");
       return;
     }
     setCounter(counter - 1);
-  }
+  };
 
   return (
     <>
---
+++ tailwind-props/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-700 mb-4">Hello World</h1>
+      <Card title = "Title1"/>
+      <Card title = "Title2"/>
+    </>
+  );
+}
+
+export default App;
